a8a5e4f58090c689b49ce0569b6e139ad910b147
David Bush <David.C.Bush@outlook.com>
2017-11-23 12:35:55 -0600

    Added login screen and preliminary app style. 1: still need to link firebase login to view/layout

--- app/src/main/java/com/ee4461l_project/recipz/InitialActivity.java
+++ app/src/main/java/com/ee4461l_project/recipz/InitialActivity.java
@@ -2,12 +2,32 @@ package com.ee4461l_project.recipz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class InitialActivity extends AppCompatActivity {
+    private EditText emailField, passwordField, usernameField;
+    private Button loginButton, registerButton;
+    private static final String TAG = "InitialActivity";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 }
