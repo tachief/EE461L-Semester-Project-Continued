e6549f4031a898b28c2287dc76aecaf2f2eb28d4
David Bush <David.C.Bush@outlook.com>
2017-11-23 10:07:13 -0600

    Initial Commit for an Android RecipZ project

---
+++ app/src/main/java/com/ee4461l_project/recipz/InitialActivity.java
@@ -0,0 +1,13 @@
+package com.ee4461l_project.recipz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
