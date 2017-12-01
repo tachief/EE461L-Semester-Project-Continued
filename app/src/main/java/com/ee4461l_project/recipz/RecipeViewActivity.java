423e30c8e6170d8a234ae721c9b822d2bd799cde
David Bush <David.C.Bush@outlook.com>
2017-12-01 16:27:24 -0600

    Added remaining activities

---
+++ app/src/main/java/com/ee4461l_project/recipz/RecipeViewActivity.java
@@ -0,0 +1,13 @@
+package com.ee4461l_project.recipz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view);
+    }
+}
