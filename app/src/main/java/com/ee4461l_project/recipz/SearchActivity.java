2f68a76f155ccced382fad7fcac1321842d02335
Tauseef <tachief@hotmail.com>
2017-12-01 18:16:12 -0600

    some work search activity

--- app/src/main/java/com/ee4461l_project/recipz/SearchActivity.java
+++ app/src/main/java/com/ee4461l_project/recipz/SearchActivity.java
@@ -2,8 +2,10 @@ package com.ee4461l_project.recipz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
     EditText editTextSearchTerms;
@@ -16,5 +18,13 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         searchButton = (Button)findViewById(R.id.button2);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                editTextSearchTerms   = (EditText)findViewById(R.id.editText);
+                testView = (TextView)findViewById(R.id.textView1);
+                testView.setText("Welcome "+testView.getText().toString()+"!");
+            }
+        });
     }
 }
