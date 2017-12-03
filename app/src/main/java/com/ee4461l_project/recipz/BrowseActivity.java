6d7f43631e6816ccd12180d61ca751011d77bc74
Tauseef <tachief@hotmail.com>
2017-12-02 18:25:58 -0600

    example of image pulled from url code

--- app/src/main/java/com/ee4461l_project/recipz/BrowseActivity.java
+++ app/src/main/java/com/ee4461l_project/recipz/BrowseActivity.java
@@ -1,7 +1,15 @@
 package com.ee4461l_project.recipz;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class BrowseActivity extends AppCompatActivity {
 
@@ -9,5 +17,38 @@ public class BrowseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        Intent intent = getIntent();
+        Bundle arg = intent.getBundleExtra("RECIPES");
+        Recipes[] rec = (Recipes[])arg.getSerializable("LIST");
+        Log.e("made it?", rec[0].getTitle());
+
+        new DownloadImageTask((ImageView) findViewById(R.id.imageView3))
+                .execute(rec[0].getImage_url());
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
