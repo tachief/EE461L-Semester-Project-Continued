58305ed9c6d1b050f65a456043863c29b0e215ba
David Bush <David.C.Bush@outlook.com>
2017-12-03 12:03:27 -0600

    Made ratings and publisher info pretty :)

--- app/src/main/java/com/ee4461l_project/recipz/BrowseActivity.java
+++ app/src/main/java/com/ee4461l_project/recipz/BrowseActivity.java
@@ -4,22 +4,22 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.io.InputStream;
-import java.text.DecimalFormat;
 
 public class BrowseActivity extends AppCompatActivity {
     TextView recipeTitles[] = new TextView[10];
     CardView recipeCards[] = new CardView[10];
     TextView recipePublishers[] = new TextView[10];
-    TextView recipeRanks[] = new TextView[10];
+    RatingBar recipeRanks[] = new RatingBar[10];
     Recipes[] rec;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -205,11 +205,10 @@ public class BrowseActivity extends AppCompatActivity {
             }
         });
 
-        DecimalFormat df = new DecimalFormat("#.0");
         for(int i = 0; i <= 9; i++) {
             recipeTitles[i].setText(rec[i].getTitle());
-            recipePublishers[i].setText("From: " + rec[i].getPublisher());
-            recipeRanks[i].setText("Rank: " + df.format(rec[i].getSocial_rank()/20.0));
+            recipePublishers[i].setText(rec[i].getPublisher());
+            recipeRanks[i].setRating((float) (rec[i].getSocial_rank()/20.0));
         }
 
     }
